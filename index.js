b6b0ede304ea3b6f962c498135405b16ad68a133
raqueldebiase <raqueldebiase@gmail.com>
2022-01-13 17:00:50 +0000

    Fix: substituição do carousel em html scroll-snap por jquery

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+/*carousel upcoming*/
+
+/* tentativas com JQUERY
+$('.container_image').slick({
+  infinite: true,
+  slidesToShow: 8,
+  slidesToScroll: 8
+});
+
+*/
+
+
+$(".container_image").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,  
+  });
+
+
+
+
 /*caroussel shop */
 
 $(".carouselSlick").slick({
